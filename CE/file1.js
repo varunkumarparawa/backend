f697cd6d50a5e213d162efa0535ba09c8b3469b8
varunkumarparawa <vkabhi1103@gmail.com>
2025-02-05 10:00:06 +0530

    work

--- CE/file1.js
+++ CE/file1.js
@@ -1,3 +1,12 @@
 const fs = require('fs');
-const data=fs.readFileSync("./data.txt");
-console.log(data.toString());
+const data=fs.readFileSync("./data.txt","utf-8");
+console.log(data);
+// fs.writeFileSync("./data.txt","ce promises");
+// fs.appendFileSync("./data.txt","aktu");
+// fs.renameFileSync("./data.txt","aktu");
+console.log(data);
+if(data.match("H")){
+    console.log("H is present")
+    const changedata=data.replace("H","abes");
+    fs.writeFileSync("./data.txt",changedata);
+}
